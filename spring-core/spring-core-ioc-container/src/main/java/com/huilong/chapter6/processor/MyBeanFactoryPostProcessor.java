9b65f5ee90167ea8ac409b9114b152f5df26e419
yhl <873104692@qq.com>
2020-12-15 15:55:21 +0800

    添加 BeanFactoryPostProcessor

--- spring-core/spring-core-ioc-container/src/main/java/com/huilong/chapter6/processor/MyBeanFactoryPostProcessor.java
+++ spring-core/spring-core-ioc-container/src/main/java/com/huilong/chapter6/processor/MyBeanFactoryPostProcessor.java
@@ -2,6 +2,7 @@ package com.huilong.chapter6.processor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,15 @@ public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
 
+        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
 
 
+        for (String beanDefinitionName : beanDefinitionNames) {
+
+            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+
+            log.info("beanDefinitionName {} beanDefinition {} ", beanDefinitionName, beanDefinition);
+        }
 
         log.info("获取  管理 spring bean 的元数据信息 ConfigurableListableBeanFactory 成功 {}", beanFactory);
     }
