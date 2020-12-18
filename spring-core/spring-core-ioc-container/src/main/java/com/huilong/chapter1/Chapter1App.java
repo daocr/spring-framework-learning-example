2a18c559cc482047af4152830658d6d02eb5b853
yhl <873104692@qq.com>
2020-12-18 17:24:50 +0800

    添加 加载文件

--- spring-core/spring-core-ioc-container/src/main/java/com/huilong/chapter1/Chapter1App.java
+++ spring-core/spring-core-ioc-container/src/main/java/com/huilong/chapter1/Chapter1App.java
@@ -13,16 +13,20 @@ public class Chapter1App {
 
     public static void main(String[] args) {
 
-        // 加载配置
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("chapter1/application.xml");
+        //1、 class path  加载配置
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("chapter1/application.xml");
+
+        //2、 文件系统 加载
+        //  ApplicationContext applicationContext =  new FileSystemXmlApplicationContext("classpath:chapter1/application.xml");
 
         // 获取 对象
-        HelloService1 bean = classPathXmlApplicationContext.getBean(HelloService1.class);
+        HelloService1 bean = applicationContext.getBean(HelloService1.class);
 
         bean.SayHello("张三");
 
         // 关闭容器
-        classPathXmlApplicationContext.close();
+        applicationContext.close();
+
 
     }
 
