4b9514679ada8b3b7a0fbfe8a517a61e5708c66e
yhl <873104692@qq.com>
2020-12-24 16:25:54 +0800

    添加 SimpleEvaluationContextSimpleEvaluationContext 基础操作

---
+++ spring-core/spring-core-sp-el/src/main/java/com/huilong/chapter2/SimpleEvaluationContextAppChapter2.java
@@ -0,0 +1,80 @@
+package com.huilong.chapter2;
+
+import com.huilong.chapter1.dto.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.SpelParserConfiguration;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.SimpleEvaluationContext;
+
+
+/**
+ * sp el 入门
+ */
+@Slf4j
+public class SimpleEvaluationContextAppChapter2 {
+
+    public static void main(String[] args) {
+
+        SpelParserConfiguration config = new SpelParserConfiguration(true, true);
+
+        ExpressionParser parser = new SpelExpressionParser(config);
+
+        //1、 配置 SimpleEvaluationContext
+        configSimpleEvaluationContext(parser);
+
+        // 2、通过el 表达修改 bean 的数据
+        setValueV1(parser);
+
+
+    }
+
+    /**
+     * 受限制的 el 表达式
+     *
+     * @see SimpleEvaluationContext#forReadWriteDataBinding() 可读可写
+     * @see SimpleEvaluationContext#forReadOnlyDataBinding()  只读
+     * @see SimpleEvaluationContext#forPropertyAccessors      自定义范围范围
+     */
+    private static void setValueV1(ExpressionParser parser) {
+
+        SimpleEvaluationContext context = SimpleEvaluationContext.forReadWriteDataBinding().build();
+
+        Employee employee = new Employee();
+        employee.setName("张三");
+        employee.setSalary(1000F);
+
+        log.info(" 修改前 ：{}", employee);
+        // 修改对象数据
+        parser.parseExpression("name").setValue(context, employee, "赵六");
+
+        log.info(" 修改后 ：{}", employee);
+    }
+
+    /**
+     * @see SimpleEvaluationContext.Builder#withRootObject(java.lang.Object) 设置根数据对象
+     * @see SimpleEvaluationContext.Builder#withTypedRootObject(java.lang.Object, org.springframework.core.convert.TypeDescriptor) 设置根数据对象，并且明确类型
+     * @see SimpleEvaluationContext.Builder#withConversionService(org.springframework.core.convert.ConversionService) 设置数据转换支持的 详情参看  spring-core-type-conversion 项目
+     * @see SimpleEvaluationContext.Builder#withTypeConverter(org.springframework.expression.TypeConverter) 设置数据转换
+     */
+
+    private static void configSimpleEvaluationContext(ExpressionParser parser) {
+
+        Employee employee = new Employee();
+        employee.setName("张三");
+        employee.setSalary(1000F);
+
+        SimpleEvaluationContext context = SimpleEvaluationContext.forReadWriteDataBinding()
+                .withRootObject(employee)
+                .build();
+
+
+        log.info(" 修改前 ：{}", employee);
+        // 修改对象数据
+        parser.parseExpression("name").setValue(context, employee, "王五");
+
+        log.info(" 修改后 ：{}", employee);
+
+    }
+
+}
---
+++ spring-core/spring-core-sp-el/src/main/java/com/huilong/chapter2/dto/Employee.java
@@ -0,0 +1,17 @@
+package com.huilong.chapter2.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 员工
+ */
+@ToString
+@Data
+public class Employee {
+
+    private String name;
+
+    private float salary;
+
+}
