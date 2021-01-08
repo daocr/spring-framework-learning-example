45808058d74df2ea87a2c4aafcaeefc297bf3420
yhl <873104692@qq.com>
2021-01-08 10:43:14 +0800

    添加 切入点

--- spring-core/spring-core-aop/src/main/java/com/huilong/chapter1/ProxyFactoryApp.java
+++ spring-core/spring-core-aop/src/main/java/com/huilong/chapter1/ProxyFactoryApp.java
@@ -7,7 +7,9 @@ import com.huilong.chapter1.advice.MyThrowsAdvice;
 import com.huilong.chapter1.service.HelloService;
 import com.huilong.chapter1.service.impl.HelloServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 
 /**
  * @author daocr
@@ -20,7 +22,15 @@ public class ProxyFactoryApp {
 
         ProxyFactory helloProxyFactory = new ProxyFactory(new HelloServiceImpl());
         helloProxyFactory.setInterfaces(HelloService.class);
-        helloProxyFactory.addAdvice(new MyMethodBeforeAdvice());
+
+        // 带切入点,前置通知
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.huilong.chapter1.service.HelloService.*(..))");
+        DefaultPointcutAdvisor defaultPointcutAdvisor = new DefaultPointcutAdvisor(pointcut, new MyMethodBeforeAdvice());
+        helloProxyFactory.addAdvisor(defaultPointcutAdvisor);
+
+
+//        helloProxyFactory.addAdvice(new MyMethodBeforeAdvice());
         helloProxyFactory.addAdvice(new MyAfterReturningAdvice());
         helloProxyFactory.addAdvice(new MyThrowsAdvice());
         helloProxyFactory.addAdvice(new MyAroundAdvice());
